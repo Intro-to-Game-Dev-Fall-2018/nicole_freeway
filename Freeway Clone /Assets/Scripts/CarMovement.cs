54332c137c10bf8af1ae6bb3485e420a4e1e3293
Nicole Shean <ns3336@nyu.edu>
2018-09-19 10:34:11 -0400

    First Commit w/ gitignore

---
+++ Freeway Clone /Assets/Scripts/CarMovement.cs
@@ -0,0 +1,61 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CarMovement : MonoBehaviour
+{
+	public bool moveRight;
+	public float speed;
+	public Vector3 carStartPos;
+	
+	void Start () 
+	{
+		
+	}
+	
+	
+	void Update ()
+	{
+		Move();
+	}
+
+	void Move()
+	{
+		if (moveRight)
+		{
+			transform.position = transform.position + Vector3.right * speed;
+		}
+		else if (!moveRight)
+		{
+			transform.position = transform.position + Vector3.left * speed;
+		}
+	}
+
+	void OnTriggerExit2D(Collider2D other)
+	{
+		if (other.gameObject.tag == "LeftBounds")
+		{
+			if (!moveRight)
+			{
+				Debug.Log("Exit Left");
+				Reset();
+			}
+		}
+		else if (other.gameObject.tag == "RightBounds")
+		{
+			if (moveRight)
+			{
+				Debug.Log("Exit Right");
+				Reset();
+			}
+		}
+	}
+
+
+	void Reset()
+	{
+		transform.position = carStartPos;
+	}
+		
+		
+}
---
+++ Freeway Clone /Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,132 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+using UnityEngine.UI;
+
+public class PlayerMovement : MonoBehaviour
+{
+	public Vector3 player1Start;
+	public Vector3 player2Start;
+	public float speed = 1;
+	public GameObject p1WinScreen;
+	public GameObject p2WinScreen;
+	public GameObject winOverlay;
+	//public GameObject p1;
+	//public GameObject p2;
+
+	public Text p1ScoreText;
+	public Text p2ScoreText;
+	
+	//private TextMeshPro p1ScoreText;
+	//private TextMeshPro p2ScoreText;
+	private int p1ScoreCount;
+	private int p2ScoreCount;
+	
+	void Start ()
+	{
+		Reset();
+		p1WinScreen.SetActive(false);
+		p2WinScreen.SetActive(false);
+		winOverlay.SetActive(false);
+		//p1ScoreText = p1.GetComponent<TextMeshPro>();
+		//p2ScoreText = p2.GetComponent<TextMeshPro>();
+		//p1ScoreText.text = "0";
+		//p2ScoreText.text = "0";
+	}
+
+	void FixedUpdate()
+	{
+		p1ScoreText.text = p1ScoreCount.ToString();
+		p2ScoreText.text = p2ScoreCount.ToString();
+		Debug.Log("Player 1: " + p1ScoreCount);
+		Debug.Log("Player 2: " + p2ScoreCount);
+	}
+	
+	void Update ()
+	{
+		
+		if (gameObject.tag == "Player1")
+		{
+			if (Input.GetKey(KeyCode.W))
+			{
+				transform.position = transform.position + Vector3.up*speed;
+			}
+			if (Input.GetKey(KeyCode.S))
+			{
+				transform.position = transform.position + Vector3.down*speed;
+			}
+		}
+		else if (gameObject.tag == "Player2")
+		{
+			if (Input.GetKey(KeyCode.UpArrow))
+			{
+				transform.position = transform.position + Vector3.up*speed;
+			}
+			if (Input.GetKey(KeyCode.DownArrow))
+			{
+				transform.position = transform.position + Vector3.down*speed;
+			}
+		}
+	}
+
+	void OnTriggerEnter2D(Collider2D other)
+	{
+		if (other.gameObject.tag == "Car")
+		{
+			//Debug.Log("hit car");
+			Reset();
+		}
+		else if (other.gameObject.tag == "Goal")
+		{
+			//Debug.Log(gameObject.tag + " reached Goal");
+			GivePoint();
+			Reset();
+		}
+	}
+
+	void Reset()
+	{
+		if (gameObject.tag == "Player1")
+		{
+			transform.position = player1Start;
+		}
+		if (gameObject.tag == "Player2")
+		{
+			transform.position = player2Start;
+		}
+	}
+
+	void GivePoint()
+	{
+		if (gameObject.tag == "Player1")
+		{
+			p1ScoreCount = p1ScoreCount + 1;
+			if (p1ScoreCount >= 5)
+			{
+				p1Wins();
+			}
+		}
+		if (gameObject.tag == "Player2")
+		{
+			p2ScoreCount = p2ScoreCount + 1;
+			if (p2ScoreCount >= 5)
+			{
+				winOverlay.SetActive(true);
+				p2Wins();
+			}
+		}
+	}
+
+	void p1Wins()
+	{
+		p1WinScreen.SetActive(true);
+		Reset();
+	}
+
+	void p2Wins()
+	{
+		p2WinScreen.SetActive(true);
+		Reset();
+	}
+}
